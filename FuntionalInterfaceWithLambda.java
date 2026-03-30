96f8fdc308ff9bf0a9fa2251f91bea30a5737058
Annanya Mathur <68476475+annanya-mathur@users.noreply.github.com>
2026-03-30 23:48:21 +0530

    Implement functional interface with lambda expression

---
+++ FuntionalInterfaceWithLambda.java
@@ -0,0 +1,15 @@
+@FunctionalInterface
+interface A {
+    void show();
+}
+
+public class Main
+{
+	public static void main(String[] args) {
+		A obj = () -> {
+		    System.out.println("in method");
+		};
+		
+		obj.show();
+	}
+}
